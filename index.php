5e13f8a51832769d89892c0f6b52b19a6c06d75a
websyspro <cpd.emersontsa@gmail.com>
2026-08-06 21:56:22 -0300

    Release v1.0.11

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-$loader = require_once __DIR__ . '/vendor/autoload.php';
-$loader->addPsr4( "Websyspro\\Package\\", __DIR__ . "/src/" );
+//$loader = 
+require_once __DIR__ . '/vendor/autoload.php';
+// $loader->addPsr4( "Websyspro\\Package\\", __DIR__ . "/src/" );
 
 use Websyspro\Package\UpdateComposer;
 new UpdateComposer( __DIR__ );
